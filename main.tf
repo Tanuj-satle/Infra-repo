fae627913cc1d388144e47005740c6972b8f9f04
Tanuj Satle <tanuj.satle@gmail.com>
2025-02-08 14:28:41 +0530

    added repo3 rg

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-resource "azurerm_resource_group" "rg-repo" {
-  name     = "rg-repo"
+resource "azurerm_resource_group" "rg-repo1" {
+  name     = "rg-repo1"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "rg-repo1" {
-  name     = "rg-repo"
+resource "azurerm_resource_group" "rg-repo2" {
+  name     = "rg-repo2"
   location = "central india"
+}
+
+resource "azurerm_resource_group" "rg-repo2" {
+  name     = "rg-repo2"
+  location = "west india"
 }
